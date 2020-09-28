7715b4b96b582cafe4e388fdd0be9361a144cded
rajarameshmamidi <42936846+rajarameshmamidi@users.noreply.github.com>
2020-09-28 13:35:40 +0530

    Add files via upload

---
+++ programs/using_camera.py
@@ -0,0 +1,45 @@
+# This program used to work with camera using kivy_garden.camera library
+# git link for kivy_garden:- https://github.com/kivy-garden/xcamera
+
+from kivy.app import App
+from kivy.lang import Builder
+
+kv = """
+#:import XCamera kivy_garden.xcamera.XCamera
+
+FloatLayout:
+    orientation: 'vertical'
+
+    XCamera:
+        id: xcamera
+        on_picture_taken: app.picture_taken(*args)
+
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint: 1, None
+        height: sp(50)
+
+        #Button:
+        #    text: 'Set landscape'
+        #    on_release: xcamera.force_landscape()
+
+        #Button:
+        #    text: 'Restore orientation'
+        #    on_release: xcamera.restore_orientation()
+"""
+
+
+class CameraApp(App):
+    def build(self):
+        return Builder.load_string(kv)
+
+    def picture_taken(self, obj, filename):
+        print('Picture taken and saved to {}'.format(filename))
+
+
+def main():
+    CameraApp().run()
+
+
+if __name__ == '__main__':
+    main()
